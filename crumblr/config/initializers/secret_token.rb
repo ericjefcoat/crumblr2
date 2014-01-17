73de204595d5aeb9930e2da47cc0e0a3b97e7308
ericjefcoat <ericjefcoat@centurylink.net>
2014-01-17 10:29:22 -0700

    Add crumblr directory

---
+++ crumblr/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crumblr::Application.config.secret_key_base = 'd3fcfbed3a45c3d31a22e2ba6387fa3931e395f242bef07afdb8028add3f870b4386ac96e6fef73275348236768c499cd76dc258059cd284901f0c03ad1e7bb0'
